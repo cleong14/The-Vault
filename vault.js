31122f1b2c0cbfae767cea326afd585ba02b4972
Chaz Leong <cleong14@gmail.com>
2015-11-07 11:29:03 -1000

    got all tests passing by making sure i referred back to the object with dot notation and this instead of living only within the function itself

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
+
+  var vaultValue = {
+
+    setValue: function (key, value) {
+      this.key = value;
+      return this.key;
+    },
+
+    getValue: function (key) {
+      if(!this.key) {
+        return null;
+      }
+      return this.key;
+    }
+  };
+
+  return vaultValue;
 };
